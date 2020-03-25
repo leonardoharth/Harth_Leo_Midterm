2de9d9e3c114ad0d98d9f2d6f26daca6bf19d46f
Leo <48158850+leonardoharth@users.noreply.github.com>
2020-03-25 16:28:37 -0400

    zczc

--- js/main.js
+++ js/main.js
@@ -18,7 +18,39 @@ var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{
 Initial data import and filters
 ===================== */
 
-var dataset = "https://data.seattle.gov/resource/7rac-kyay.json";
+var dataset = energy;
+
+// APPLY FILTERS
+
+var markers = [];
+
+var parsedDF = function(data) {
+  var allBuildings = JSON.parse(data);
+  var markerCoordinates = allBuildings.map(function(building) {
+  var coords = {
+      lat: building.latitude,
+      long: building.longitude
+};
+  return coords;
+});
+  return markerCoordinates;
+};
+
+var makeMarkers = function(data) {
+  markers = data.map(function(coords){
+    return L.marker([coords.lat, coords.long]);
+  });
+  return markers;
+};
+
+var plotMarkers = function(markers) {
+  markers.forEach(function(marker){
+    return marker.addTo(map);
+  });
+};
+
+/*
+
 var featureGroup;
 
 var myStyle = function(feature) {
@@ -26,26 +58,25 @@ var myStyle = function(feature) {
 };
 
 var showResults = function() {
-  /* =====================
-  This function uses some jQuery methods that may be new. $(element).hide()
-  will add the CSS "display: none" to the element, effectively removing it
-  from the page. $(element).show() removes "display: none" from an element,
-  returning it to the page. You don't need to change this part.
-  ===================== */
+
+//  This function uses some jQuery methods that may be new. $(element).hide()
+//  will add the CSS "display: none" to the element, effectively removing it
+//  from the page. $(element).show() removes "display: none" from an element,
+//  returning it to the page. You don't need to change this part.
+
   // => <div id="intro" css="display: none">
   $('#intro').hide();
   // => <div id="results">
   $('#results').show();
 };
 
-
 var eachFeatureFunction = function(layer) {
   layer.on('click', function (event) {
-    /* =====================
-    The following code will run every time a layer on the map is clicked.
-    Check out layer.feature to see some useful data about the layer that
-    you can use in your application.
-    ===================== */
+
+  //  The following code will run every time a layer on the map is clicked.
+  //  Check out layer.feature to see some useful data about the layer that
+  //  you can use in your application.
+
     console.log(layer.feature);
     showResults();
   });
@@ -67,3 +98,5 @@ $(document).ready(function() {
     featureGroup.eachLayer(eachFeatureFunction);
   });
 });
+
+*/
